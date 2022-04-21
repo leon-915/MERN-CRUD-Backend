d1ba5acae722908d1a963972f51db66e6aef74e0
Leon C <stone92822@outlook.com>
2022-04-22 04:46:45 +0800

    4-22-1

--- routes/bulletins.js
+++ routes/bulletins.js
@@ -42,12 +42,6 @@ router.get('/:id/downvote', getBulletin, async (req, res) => {
 
 // Create one bulletin
 router.post('/', async (req, res) => {
-    const bulletin = new Bulletin({
-        title: req.body.title,
-        //content: req.body.content,
-        //tag: req.body.tag
-    });
-
     
 });
 
